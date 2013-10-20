6f3bc67c285bf95dc57aea8e90557834d593509b
Alexandr Ivaschenko <a.ivashchenko@nemotelecom.co.uk>
2013-10-20 10:07:09 +0400

    [+] Форма редактирования/добавления еды

--- app/controllers/food/AdminFood.php
+++ app/controllers/food/AdminFood.php
@@ -2,8 +2,11 @@
 
 class Adminfood extends AuthController {
    public function edit(){
-        return array();
-      }
+      if(isset($_GET['id']) && !empty($_GET['id'])){
+         $fm = new FoodModel();
+         return $fm->getById($_GET['id']);
+      } else return array();
+   }
 
    public function check_edit(){
       return array();
